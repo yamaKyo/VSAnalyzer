a6c5779c1b2de751850cb0af7745256d3775d056
Kyohei <software.engineering.unics@gmail>
2021-07-17 17:35:51 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Build.Construction;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace VSAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("[Usage] VSAnalyzer.exe <slnファイル>");
+                return;
+            }
+
+
+
+            var solutionFile = Path.GetFullPath(args[0]);
+
+            if (!File.Exists(solutionFile))
+            {
+                Console.WriteLine("Error solution file not found");
+                return;
+            }
+
+            var solutionDir = Path.GetDirectoryName(solutionFile);
+            var solution = SolutionFile.Parse(solutionFile);
+
+            foreach(var project in solution.ProjectsInOrder)
+            {
+                var projectDir = Path.GetDirectoryName(project.AbsolutePath);
+                var outputDir =  "./" + project.ProjectName;
+
+                if (!Directory.Exists(outputDir))
+                {
+                    Directory.CreateDirectory(outputDir);
+                }
+
+                if (!File.Exists(project.AbsolutePath))
+                {
+                    Console.WriteLine("Error project file not found");
+                    continue;
+                }
+
+                var config = VCppConfig.Parse(project.AbsolutePath);
+
+                foreach (var name in config.get_config_names())
+                {
+                    var output_file = Path.Combine(outputDir, "compile_flags_" + name.Replace('|', '_') +".txt");
+                    using (StreamWriter sw = new StreamWriter(output_file))
+                    {
+                        foreach (var i in config.get_includes(name).Where(i => !i.StartsWith("%(")))
+                        {
+                            string f = solve_project_dir(solve_solution_dir(i, solutionDir), projectDir);
+                            sw.WriteLine("-I" + f);
+                        }
+                        foreach (var i in config.get_definitions(name).Where(i => !i.StartsWith("%(")))
+                        {
+                            sw.WriteLine("-D" + i);
+                        }
+                    }
+                }
+            }
+        }
+
+        static string solve_solution_dir(string path, string solution_dir)
+        {
+            return path.Replace("$(SolutionDir)", solution_dir);
+        }
+
+        static string solve_project_dir(string path, string project_dir)
+        {
+            return path.Replace("$(ProjectDir)", project_dir);
+        }
+
+    }
+
+
+}
---
+++ VCppConfig.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Construction;
+using System.Xml.Linq;
+
+
+namespace VSAnalyzer
+{
+    class VCppConfig
+    {
+        private readonly XDocument doc;
+        private readonly XElement root;
+        private readonly IDictionary<string, XElement> configs;
+
+        private VCppConfig(XDocument doc)
+        {
+            this.doc = doc;
+            this.root = this.doc.Root;
+            var configs = root.Descendants(root.GetDefaultNamespace() + "ItemDefinitionGroup");
+            this.configs = new Dictionary<string, XElement>();
+            
+            foreach(var c in configs)
+            {
+                var x = c.Attribute("Condition").Value.Split("==")[1].Trim('\'').Trim();
+                this.configs.Add(x, c);
+            }
+        }
+
+        public static VCppConfig Parse(string name)
+        {
+            var doc = XDocument.Load(name);
+
+            if(doc != null)
+            {
+                return new VCppConfig(doc);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<string> get_config_names()
+        {
+            return this.configs.Keys;
+        }
+
+        public ISet<string> get_includes(string config_name)
+        {
+            return search_element(config_name, "AdditionalIncludeDirectories", ";");
+        }
+
+        public ISet<string> get_definitions(string config_name)
+        {
+            return search_element(config_name, "PreprocessorDefinitions", ";");
+        }
+
+        private ISet<string> search_element(string config_name, string element_name, string spliter = "")
+        {
+            if (this.configs.ContainsKey(config_name))
+            {
+                var element = this.configs[config_name];
+                var n = element.GetDefaultNamespace();
+                ISet<string> ret = new HashSet<string>();
+                foreach (var e in element.Descendants(n + element_name))
+                {
+                    var val = e.Value;
+
+                    if (spliter.Length > 0)
+                    {
+                        foreach (var i in val.Split(";"))
+                        {
+                            ret.Add(i);
+                        }
+                    }
+                    else
+                    {
+                        ret.Add(val);
+                    }
+                }
+                return ret;
+            }
+            else
+            {
+                return new HashSet<string>();
+            }
+        }
+    }
+}
